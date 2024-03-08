b95bc200ccfd10d31d38ba0490668190ac8e295b
Inayshi <107098935+Inayshi@users.noreply.github.com>
2024-03-08 10:03:44 +0800

    added buttons

--- resources/views/cms.blade.php
+++ resources/views/cms.blade.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap');
 
@@ -33,6 +34,9 @@
             background-color: #2C2929;
         }
 
+        .form-group {
+           line-height: 50px;
+        }
         label {
             color: #C8C6BA;
         }
@@ -52,6 +56,35 @@
             color: #C8C6BA !important;
             opacity: 1;
         }
+
+        .experience-container{
+            line-height: 1.5    px;  
+        }
+
+
+        .experience-container p {
+            color: #C8C6BA;
+            font-size: 14px;
+        }
+
+        hr {
+            background-color: #C8C6BA;
+            margin-top: 20px;
+        }
+        
+       
+
+        .btn {
+            background-color: transparent; 
+            border: none; 
+            padding: 0;
+        }
+
+        .btn i {
+            color: #C8C6BA; 
+        }
+
+
     </style>
     <title>CMS</title>
 </head>
@@ -66,6 +99,7 @@
                         <div class="form-group">
                             <label for="summary">Summary</label>
                             <input type="text" class="form-control" id="summary" placeholder="Enter Summary">
+                            <button type="button" class="btn btn-light px-3" style="font-family: 'Urbanist', sans-serif; font-weight: bold; background-color: #C8C6BA; padding-top: 3px;">Save</button>
                         </div>
                     </form>
                 </div>
@@ -81,8 +115,25 @@
                             <textarea class="form-control" id="position" rows="1" placeholder="Enter Position"></textarea>
                             <label for="experiences">Date</label>
                             <textarea class="form-control" id="date" rows="1" placeholder="Enter date started and ended"></textarea>
+                            <button type="button" class="btn btn-light px-3" style="font-family: 'Urbanist', sans-serif; font-weight: bold; background-color: #C8C6BA; padding: 3px;">Add</button>
+
                         </div>
                     </form>
+                    <hr>
+                    <div class="experience-container">
+                        <div class="experience-text">
+                            <p>
+                                Zamboanga City Hall - Computer Services Division<br>
+                                Software Development Intern<br>
+                                February - March 2024
+                            </p>
+                        </div>
+                        <div class="experience-buttons">
+                            <button type="button" class="btn btn-warning px-1"><i class="fas fa-edit" aria-hidden="true"></i></button>
+                            <button type="button" class="btn btn-danger px-1"><i class="fas fa-trash-alt" aria-hidden="true"></i></button>
+                        </div>
+                    </div>
+                    <hr>
                 </div>
                 <div class="shadow-container">
                     <form>
@@ -94,8 +145,24 @@
                             <textarea class="form-control" id="position" rows="1" placeholder="Enter Position"></textarea>
                             <label for="experiences">Date</label>
                             <textarea class="form-control" id="date" rows="1" placeholder="Enter date started and ended"></textarea>
+                            <button type="button" class="btn btn-light px-3" style="font-family: 'Urbanist', sans-serif; font-weight: bold; background-color: #C8C6BA; padding: 3px;">Add</button>
+
                         </div>
                     </form>
+                    <hr>
+                    <div class="experience-container">
+                        <div class="experience-text">
+                            <p>
+                                Google Developer Student Clubs Crimsons<br>
+                                CEO and Lead<br>
+                                August 2023 - July 2024
+                            </p>
+                        </div>
+                        <div class="experience-buttons">
+                            <button type="button" class="btn btn-warning px-1"><i class="fas fa-edit" aria-hidden="true"></i></button>
+                            <button type="button" class="btn btn-danger px-1"><i class="fas fa-trash-alt" aria-hidden="true"></i></button>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div class="col-md-6">
@@ -105,15 +172,19 @@
                         <div class="form-group">
                             <label for="github">GitHub</label>
                             <input type="text" class="form-control" id="github" placeholder="Enter GitHub link">
+                            <button type="button" class="btn btn-light px-3" style="font-family: 'Urbanist', sans-serif; font-weight: bold; background-color: #C8C6BA; padding-top: 3px;">Save</button>
                         </div>
                         <div class="form-group">
                             <label for="behance">Behance</label>
                             <input type="text" class="form-control" id="behance" placeholder="Enter Behance link">
+                            <button type="button" class="btn btn-light px-3" style="font-family: 'Urbanist', sans-serif; font-weight: bold; background-color: #C8C6BA; padding-top: 3px;">Save</button>
                         </div>
                         <div class="form-group">
                             <label for="linkedin">LinkedIn</label>
                             <input type="text" class="form-control" id="linkedin" placeholder="Enter LinkedIn link">
+                            <button type="button" class="btn btn-light px-3" style="font-family: 'Urbanist', sans-serif; font-weight: bold; background-color: #C8C6BA; padding-top: 3px;">Save</button>
                         </div>
+                        
                     </form>
                 </div>
                 <div class="shadow-container">
@@ -141,6 +212,7 @@
                                 <textarea class="form-control" id="position" rows="3" placeholder="Project Summary"></textarea>
                                 <label for="experiences">Link</label>
                                 <textarea class="form-control" id="date" rows="1" placeholder="Link to Github repository"></textarea>
+                                <button type="button" class="btn btn-light px-3" style="font-family: 'Urbanist', sans-serif; font-weight: bold; background-color: #C8C6BA; padding: 3px;">Add</button>
                             </div>
                         </form>
                     </div>               
@@ -156,6 +228,7 @@
                                 <textarea class="form-control" id="position" rows="3" placeholder="Project Summary"></textarea>
                                 <label for="experiences">Link</label>
                                 <textarea class="form-control" id="date" rows="1" placeholder="Link to Behance Post"></textarea>
+                                <button type="button" class="btn btn-light px-3" style="font-family: 'Urbanist', sans-serif; font-weight: bold; background-color: #C8C6BA; padding-top: 3px;">Add</button>
                             </div>
                         </form>
                     </div>               
